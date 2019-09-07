991e137056c150babb8d72a76560a8126f98ff70
Jacob <zhangen69@gmail.com>
2019-09-07 15:26:54 +0800

    update solution

--- node-app/src/models/pointTransaction.model.ts
+++ node-app/src/models/pointTransaction.model.ts
@@ -1,8 +1,15 @@
 import mongoose from 'mongoose';
 import auditable from './auditable.model';
 
-const schema = new mongoose.Schema({});
+const PointTransactionSchema = new mongoose.Schema({
+    transferFrom: { type: mongoose.Types.ObjectId, ref: 'User' },
+    transferTo: { type: mongoose.Types.ObjectId, ref: 'User' },
+    points: { type: Number, default: 0 },
+    type: { type: String, enum: ['Transfer', 'Reward', 'Award'], required: true },
+    source: { type: String, enum: ['Post', 'GameReward', 'ManualAward', 'BotAward', 'ClaimableAward'], required: true },
+    sourceId: { type: mongoose.Types.ObjectId },
+});
 
-schema.add(auditable);
+PointTransactionSchema.add(auditable);
 
-export default mongoose.model('PointTransaction', schema);
+export default mongoose.model('PointTransaction', PointTransactionSchema);
--- node-app/src/models/post.model.ts
+++ node-app/src/models/post.model.ts
@@ -1,8 +1,29 @@
 import mongoose from 'mongoose';
 import auditable from './auditable.model';
 
-const schema = new mongoose.Schema({});
+const PostLikeSchema = new mongoose.Schema({
+    likedBy: { type: mongoose.Types.ObjectId, ref: 'User' },
+    createdDate: { type: Date, default: Date.now },
+    updatedDate: { type: Date, default: Date.now },
+});
 
-schema.add(auditable);
+const PostCommentSchema = new mongoose.Schema({
+    commentedBy: { type: mongoose.Types.ObjectId, ref: 'User' },
+    message: { type: String },
+    createdDate: { type: Date, default: Date.now },
+    updatedDate: { type: Date, default: Date.now },
+});
 
-export default mongoose.model('Post', schema);
+const PostSchema = new mongoose.Schema({
+    postedBy: { type: mongoose.Types.ObjectId, ref: 'User' },
+    message: { type: String },
+    hashtags: { type: [String], default: [] },
+    points: { type: Number, default: 0 },
+    givePointsTo: { type: mongoose.Types.ObjectId, ref: 'User' },
+    likes: { type: [PostLikeSchema], default: [] },
+    comments: { type: [PostCommentSchema], default: [] },
+});
+
+PostSchema.add(auditable);
+
+export default mongoose.model('Post', PostSchema);
