26a843fa4bd96b6d0a758f76fdaed8c846658075
Jacob <zhangen69@gmail.com>
2019-09-13 08:00:40 +0800

    update solution (awards)

--- ng-app/projects/ng-app/src/app/admin/awards/manual-form/manual-form.component.ts
+++ ng-app/projects/ng-app/src/app/admin/awards/manual-form/manual-form.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manual-form.component.scss']
 })
 export class ManualFormComponent implements OnInit {
+  includes = [];
+  fields = [
+    { name: 'role', type: 'string', required: true },
+    { name: 'receiver', type: 'string', required: true },
+    { name: 'message', type: 'textarea', required: true },
+    { name: 'bonus', type: 'number', required: true },
+  ];
 
   constructor() { }
 
--- ng-app/projects/ng-app/src/app/admin/awards/manual-list/manual-list.component.ts
+++ ng-app/projects/ng-app/src/app/admin/awards/manual-list/manual-list.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manual-list.component.scss']
 })
 export class ManualListComponent implements OnInit {
+  // showDefaultBtn = false;
+  columns = [
+    { name: 'displayName', format: 'link', link: '/staff/view' },
+    { name: 'email' },
+    { name: 'phoneNumber' },
+  ];
+  filterList = [];
+  actions = [];
 
   constructor() { }
 
