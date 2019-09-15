eaeba8aa621ab8caa0350527e71e363bee86e2a6
Jacob Tan <zhangen69@live.co.uk>
2019-09-15 18:16:42 +0800

    Update award.model.ts

--- node-app/src/models/award.model.ts
+++ node-app/src/models/award.model.ts
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose';
 import auditable from './auditable.model';
 
-const schema = new mongoose.Schema({});
+const schema = new mongoose.Schema({
+    role: { type: String, required: true },
+    bonus: { type: Number, required: true },
+    receiver: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+    message: { type: String, required: true },
+});
 
 schema.add(auditable);
 
