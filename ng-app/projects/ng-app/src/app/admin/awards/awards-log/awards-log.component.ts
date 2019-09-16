d09b4aa73240c07a565192bcf0cb4ab4d7656aad
Jacob <zhangen69@gmail.com>
2019-09-16 09:59:19 +0800

    uipdate solution

--- ng-app/projects/ng-app/src/app/admin/awards/awards-log/awards-log.component.ts
+++ ng-app/projects/ng-app/src/app/admin/awards/awards-log/awards-log.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IStandardColumn } from '../../../shared/standard/standard-form-field.interface';
 
 @Component({
   selector: 'app-awards-log',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./awards-log.component.scss']
 })
 export class AwardsLogComponent implements OnInit {
+  baseUrl = '/admin/awards/awards-log';
+  includes = ['sender', 'receiver'];
+  columns: IStandardColumn[] = [
+    { name: 'sender.displayName' },
+    { name: 'receiver.displayName' },
+    { name: 'points' },
+    { name: 'type' },
+    { name: 'source' },
+  ];
 
   constructor() { }
 
--- node-app/src/models/pointTransaction.model.ts
+++ node-app/src/models/pointTransaction.model.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 import auditable from './auditable.model';
 
 const PointTransactionSchema = new mongoose.Schema({
-    transferFrom: { type: mongoose.Types.ObjectId, ref: 'User' },
-    transferTo: { type: mongoose.Types.ObjectId, ref: 'User' },
+    sender: { type: mongoose.Types.ObjectId, ref: 'User' },
+    receiver: { type: mongoose.Types.ObjectId, ref: 'User' },
     points: { type: Number, default: 0 },
     type: { type: String, enum: ['Transfer', 'Reward', 'Award'], required: true },
     source: { type: String, enum: ['Post', 'GameReward', 'ManualAward', 'BotAward', 'ClaimableAward'], required: true },
