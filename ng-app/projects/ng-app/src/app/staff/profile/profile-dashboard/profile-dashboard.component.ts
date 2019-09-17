7d3371c05117b7808db33c37b022794f937960f2
Jacob <zhangen69@gmail.com>
2019-09-18 02:52:19 +0800

    update profile

--- ng-app/projects/ng-app/src/app/staff/profile/profile-dashboard/profile-dashboard.component.ts
+++ ng-app/projects/ng-app/src/app/staff/profile/profile-dashboard/profile-dashboard.component.ts
@@ -1,6 +1,9 @@
 import { environment } from './../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-dashboard',
@@ -8,19 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-dashboard.component.scss']
 })
 export class ProfileDashboardComponent implements OnInit {
-  profile: any;
   bonusProfile: any;
   apiUrl = environment.apiUrl;
+  profile = this.formBuilder.group({
+    _id: [null, Validators.required],
+    username: [null, Validators.required],
+    displayName: ['', Validators.required],
+    email: ['', Validators.required],
+    phoneNumber: ['', Validators.required],
+    avatarImageUrl: [''],
+    signatureImageUrl: ['']
+  });
+  imagesPreview: any = {};
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, 
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService,
+    private router: Router) { }
 
   ngOnInit() {
     this.http.get(this.apiUrl + '/service/user/fetchProfile').subscribe(({ data }: any) => {
-      this.profile = data;
+      this.profile.patchValue(data);
       this.http.get(this.apiUrl + '/service/bonus/profile').subscribe((data) => {
         this.bonusProfile = data;
       });
     });
+  }
+
+  onSubmit(formData) {
+    this.http
+      .put(this.apiUrl + '/service/user/updateProfile', formData)
+      .subscribe((res: any) => {
+        this.toastr.success(res.message);
+        window.location.reload();
+      });
   }
 
 }
